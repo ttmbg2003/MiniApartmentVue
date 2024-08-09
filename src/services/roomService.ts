d46d7926fecef67c9e9ea00dc37aaae09941396d
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-08-09 14:46:18 +0700

    Update roomService.ts

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -2,8 +2,10 @@ import type { Room } from "@/type/Room";
 import apiClient from "@/utils/apiClient";
 
 class RoomService {
-  getAllRoomAvailable = async (): Promise<any> => {
-    const response = await apiClient.get(`/api/room/getRoomAvailable`);
+  getAllRoomAvailable = async (month: number, year: number): Promise<any> => {
+    const response = await apiClient.get(
+      `/api/room/getRoomAvailable?month=${month}&year=${year}`
+    );
     return response.data["result"];
   };
   getAllRoom = async (): Promise<any> => {
